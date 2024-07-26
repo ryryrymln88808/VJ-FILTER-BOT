e9259e85743a10d1016d443ddc961b4735733c88
ryryrymln88808 <ryryrymln88808@gmail.com>
2024-07-26 11:06:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "above-traci-none1718-61e24ce9.koyeb.app")
+URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
